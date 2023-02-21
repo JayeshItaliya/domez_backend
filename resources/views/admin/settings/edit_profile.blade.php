5c91e54056bdaf7b9aa5bf9a23eada19c80b9263
JayeshItaliya <jayeshmitaliya@gmail.com>
2023-02-21 17:36:59 +0530

    changes

--- resources/views/admin/settings/edit_profile.blade.php
+++ resources/views/admin/settings/edit_profile.blade.php
@@ -2,14 +2,14 @@
 @section('styles')
 @endsection
 @section('title')
-    add league
+      Edit Profile
 @endsection
 @section('contents')
     <!-- Title -->
     <div class="card mb-3">
         <div class="card-body py-2">
             <div class="d-flex align-items-center justify-content-between">
-                <p class="text-secondary fw-semibold">add league</p>
+                <p class="text-secondary fw-semibold">Edit Profile</p>
                 <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='10' height='10'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);"
                     aria-label="breadcrumb">
                     <ol class="breadcrumb m-0">
@@ -26,8 +26,8 @@
                                 </svg>
                             </a>
                         </li>
-                        <li class="breadcrumb-item ">league</li>
-                        <li class="breadcrumb-item active" aria-current="page">add league</li>
+                        <li class="breadcrumb-item">General Settings</li>
+                        <li class="breadcrumb-item active" aria-current="page">Edit Profile</li>
                     </ol>
                 </nav>
             </div>
@@ -39,167 +39,62 @@
         <div class="card-body">
             <div class="row">
                 <div class="col-md-6">
-                    <label for="" class="form-label">Select Sports</label>
-                    <div class="radio-box d-flex">
-                        <div class="form-check pe-3">
-                            <input type="checkbox" name="benefits[]" class="form-check-input" value="Parking" id="Parking">
-                            <label class="form-check-label" for="flexRadioDefault1">
-                                Vollyball
-                            </label>
-                        </div>
-                        <div class="form-check pe-3">
-                            <input type="checkbox" name="benefits[]" class="form-check-input" value="Parking" id="Parking">
-                            <label class="form-check-label" for="flexRadioDefault2">
-                                Golf
-                            </label>
-                        </div>
-                        <div class="form-check pe-3">
-                            <input type="checkbox" name="benefits[]" class="form-check-input" value="Parking" id="Parking">
-                            <label class="form-check-label" for="flexRadioDefault2">
-                                Tennis
-                            </label>
-                        </div>
-                        <div class="form-check pe-3">
-                            <input type="checkbox" name="benefits[]" class="form-check-input" value="Parking" id="Parking">
-                            <label class="form-check-label" for="flexRadioDefault2">
-                                Soccer
-                            </label>
-                        </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1"
+                            placeholder="Jone">
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">League Name</label>
+                        <label for="exampleFormControlInput1" class="form-label">Email Address</label>
                         <input type="email" class="form-control" id="exampleFormControlInput1"
-                            placeholder="Enter League Name">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="mb-3">
-                                <label class="form-label">Dome</label>
-                                <select class="form-control mt-2" id="inlineFormSelectPref">
-                                    <option selected>Select Dome</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                    <label for="inlineFormSelectPref" class="form-label"><i
-                                            class="fa-solid fa-angle-down"></i></label>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="mb-3">
-                                <label class="form-label">Field</label>
-                                <select class="form-control mt-2" id="inlineFormSelectPref">
-                                    <option selected>Select Field</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                    <label for="inlineFormSelectPref" class="form-label"><i
-                                            class="fa-solid fa-angle-down"></i></label>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <div class="mb-3">
-                                <label class="form-label">Start Date</label>
-                                <input type="text" class="form-control date mt-2" id="datepicker" value="00/00/0000">
-                            </div>
-                        </div>
-                        <div class="col-md-3">
-                            <div class="mb-3">
-                                <label class="form-label">End Date</label>
-                                <input type="text" class="form-control date mt-2" id="datepicker" value="00/00/0000">
-                            </div>
-                        </div>
-                        <div class="col-md-3">
-                            <div class="mb-3">
-                                <label class="form-label">Start Time</label>
-                                <input type="time" class="form-control mt-2" value="00:00 AM" />
-                            </div>
-                        </div>
-                        <div class="col-md-3">
-                            <div class="mb-3">
-                                <label class="form-label">End Time</label>
-                                <input type="time" class="form-control mt-2" value="00:00 AM" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="mb-3">
-                                <label class="form-label">Select Age</label>
-                                <select class="form-control mt-2" id="inlineFormSelectPref">
-                                    <option selected>Select Age</option>
-                                    <option value="1">17</option>
-                                    <option value="2">21</option>
-                                    <option value="3">25</option>
-                                    <label for="inlineFormSelectPref" class="form-label"><i
-                                            class="fa-solid fa-angle-down"></i></label>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="mb-3">
-                                <label class="form-label">Select Gender</label>
-                                <select class="form-control mt-2" id="inlineFormSelectPref">
-                                    <option selected>Select Gender</option>
-                                    <option value="1">Men</option>
-                                    <option value="2">Female</option>
-                                    <label for="inlineFormSelectPref" class="form-label"><i
-                                            class="fa-solid fa-angle-down"></i></label>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="mb-3">
-                                <label class="form-label">Min Players</label>
-                                <input placeholder="Enter Min Players" class="form-control mt-2">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="mb-3">
-                                <label class="form-label">Max Players</label>
-                                <input placeholder="Enter Max Players" class="form-control mt-2">
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="mb-3">
-                                <label class="form-label">Team Limit</label>
-                                <input placeholder="Enter Team Limit" class="form-control mt-2">
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="mb-3">
-                                <label class="form-label">Price Per Team</label>
-                                <input placeholder="Enter Price" class="form-control mt-2">
-                            </div>
-                        </div>
+                            placeholder="Jone@hotmail.com">
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label class="form-label">League Banner Images</label>
+                        <label class="form-label">Profile Image(Optional)</label>
                         <input type="file" class="form-control mt-2 mb-4">
                         <div class="add-league-img mt-2">
-                            <img src="http://localhost/domez/storage/app/public/admin/images/League/Add_League.png">
+                            <img src="http://localhost/domez_backend/storage/app/public/admin/images/gernral_settings/edit_profile.png">
                         </div>
                     </div>
                 </div>
                 <div class="col-md-12">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <form class="card mt-3" action="" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="p-4">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="mb-3">
+                        <label for="inputPassword" class="form-label">Current Password</label>
+                        <input type="password" class="form-control" id="inputPassword"
+                            placeholder="********">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="mb-3">
+                        <label for="inputPassword" class="form-label">New Password</label>
+                        <input type="password" class="form-control" id="inputPassword"
+                            placeholder="********">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="mb-3">
+                        <label for="inputPassword" class="form-label">Confirm Password</label>
+                        <input type="password" class="form-control" id="inputPassword"
+                            placeholder="********">
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <button type="submit" class="btn btn-primary">Save</button>
                 </div>
             </div>
         </div>
