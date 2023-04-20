955a35d0b3cc8dee9fe65a29e2b8330c924b351e
JayeshItaliya <jayeshmitaliya@gmail.com>
2023-04-20 05:27:37 +0000

    ..

--- app/Http/Controllers/api/AdminController.php
+++ app/Http/Controllers/api/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
         Artisan::call('cache:clear');
         Artisan::call('route:clear');
         Artisan::call('view:clear');
-        Artisan::call('optimize');
+        // Artisan::call('optimize');
         return response()->json(["status" => 1, "message" => "Successful"], 200);
     }
 }
